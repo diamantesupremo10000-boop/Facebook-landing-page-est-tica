01459d1e7e0a981183628222b074a8476269b271
diamantesupremo10000-boop <diamante.supremo.10000@gmail.com>
2025-09-10 13:13:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Botón de llamada a la acción
+document.getElementById("cta-btn").addEventListener("click", () => {
+  alert("¡Gracias por tu interés! Tu landing page puede estar lista en minutos 🚀");
+});
